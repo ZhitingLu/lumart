ed878a915f571ba3e52a95d86e6d4b8858c71752
Zhiting Lu <zlu@asbarcelona.com>
2024-10-12 20:41:54 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'red': {
+          500: '#f43f5e',
+        },
       },
     },
   },
