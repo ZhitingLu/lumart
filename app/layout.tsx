108058547081c90d011c122dc34450945c7b417c
Zhiting Lu <zhitinglu@outlook.com>
2024-10-16 12:55:28 +0200

    use the provider without wrapping the whole app in PersistGate on the server side

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { ReactNode } from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { Persistor, persistStore } from "redux-persist";
+import ReduxProvider from "../store/Provider"; // Adjust the import path
 import "./globals.css";
-import store from "../store";
-
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,26 +19,16 @@ export const metadata: Metadata = {
   description: "Shop anything you want at LuMart",
 };
 
-// Initialize the Redux Persistor
-const persistor: Persistor = persistStore(store);
-
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
-    </PersistGate>
-    </Provider>
   );
 }
