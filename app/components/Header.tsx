732bf32d5cc1a615be5ec7e037a3767d4992f6d1
Zhiting Lu <zhitinglu@outlook.com>
2024-10-20 21:58:39 +0200

    added Heaer component

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1>LuMart</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/cart">Cart</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
