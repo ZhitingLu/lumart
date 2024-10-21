a1e2985fb5424644aedc8447cf5a97d83f34329b
Zhiting Lu <zhitinglu@outlook.com>
2024-10-21 22:15:20 +0200

    updated 'Header' component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import ReduxProvider from "../store/Provider";
 import "./globals.css";
-import Header from '../app/components/Header';
+import Header from './components/Header';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
