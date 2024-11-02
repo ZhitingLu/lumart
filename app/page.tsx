8c101a96cc314b596d211d1307e5a9c4d17f8f2e
Zhiting Lu <zhitinglu@outlook.com>
2024-11-02 08:52:20 +0100

    removed used content

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import './globals.css';
 export default function Home() {
   return (
     <>
-   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-3xl text-red-500 font-bold">Welcome to lumart</h1>
-    
-    </div>
+   
   </>
   );
 }
